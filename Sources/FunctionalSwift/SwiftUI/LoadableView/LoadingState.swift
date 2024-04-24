4765a54c3a226c5ed5047b2a6429e3f369cc3b03
Alex Crowe <alexei.hmelevski@gmail.com>
2024-04-24 16:02:19 -0400

    remove combine and swift ui from the pacakge

--- Sources/FunctionalSwift/SwiftUI/LoadableView/LoadingState.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-
-public enum LoadingState<Value> {
-    case idle
-    case loading
-    case failed(Error)
-    case loaded(Value)
-}
--- Sources/FunctionalSwift/SwiftUI/LoadableView/LoadingViewModel.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public protocol LoadableObject: ObservableObject {
-    associatedtype Output
-    var state: LoadingState<Output> { get }
-    func load()
-}
